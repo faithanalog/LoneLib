9ee5b0a2ad5d5cc62a92bdd8f48ee4e577731de6
Unknownloner <Unknownloner.Online@gmail.com>
2013-05-10 22:27:14 -0400

    FontRenderer now uses glMapBuffer/glMapBufferRange for data transfer
    Fixed Font Renderer so drawString with size X and a size Xpt font base
    will draw on the screen at size Xpt (basically fixed scaling issues)
    Switched the Font Renderer to use glMapBuffer, no need for maintain a
    buffer in ram for each Font Renderer to mirror the VBO.
    Made rendering strings < 128 length more effecient
    Fixed rendering strings > 128 length

--- src/main/java/com/unknownloner/lonelib/graphics/font/FontRenderer.java
+++ src/main/java/com/unknownloner/lonelib/graphics/font/FontRenderer.java
@@ -11,6 +11,8 @@ import java.nio.ShortBuffer;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.GLContext;
 
 import com.unknownloner.lonelib.graphics.buffers.VertexArrayObject;
 import com.unknownloner.lonelib.graphics.buffers.VertexAttribPointer;
@@ -27,13 +29,15 @@ public class FontRenderer {
 	
 	
 	public static ShaderProgram globalFontProgram = new ShaderProgram("/shaders/FontShader.vert", "/shaders/FontShader.frag");
+	private static boolean mapBufferRange = GLContext.getCapabilities().GL_ARB_map_buffer_range;
+	
 	private Texture fontTexture;
 	private ShaderProgram shaderProgram = globalFontProgram;
 	private boolean[] displayable = new boolean[256];
 	private float[] widths = new float[256];
 	private float[] texWidths = new float[256];
 	private float texHeight;
-	private FloatBuffer dataBuffer = BufferUtil.createFloatBuffer(128 * 9 * 4); //128 chars * 9 elements * 4 vertices per element
+	private float sizeOverPt;
 	private VertexBufferObject data = new VertexBufferObject(GL15.GL_ARRAY_BUFFER);
 	private VertexBufferObject inds = new VertexBufferObject(GL15.GL_ELEMENT_ARRAY_BUFFER);
 	private VertexArrayObject vao = new VertexArrayObject(
@@ -44,6 +48,7 @@ public class FontRenderer {
 	
 	public FontRenderer(Font font) {
 		int size = ImageUtil.ascent(font) + ImageUtil.descent(font);
+		sizeOverPt = size / font.getSize2D();
 		int cellSize = MathUtil.nextPowerOfTwo(size);
 		BufferedImage img = new BufferedImage(cellSize * 16, cellSize * 16, BufferedImage.TYPE_INT_ARGB);
 		texHeight = size / (float)img.getHeight();
@@ -84,6 +89,8 @@ public class FontRenderer {
 		}
 		indBuf.flip();
 		inds.bufferData(indBuf, GL15.GL_STATIC_DRAW);
+		data.assign();
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, 128 * 9 * 4 * 4, GL15.GL_STREAM_DRAW);
 	}
 	
 	public void setShaderProgram(ShaderProgram prgm, int posAttrib, int colorAttrib, int texCoordAttrib) {
@@ -101,25 +108,41 @@ public class FontRenderer {
 	}
 	
 	public void drawString(String text, Vec3 startPos, Vec4 color, float charSize) {
+		//Adjust to be accurate to the original font's point value
+		charSize *= sizeOverPt;
 		fontTexture.assign(GL13.GL_TEXTURE0);
 		shaderProgram.assign();
 		vao.assign();
 		inds.assign();
 		int strPos = 0;
 		char[] textChars = text.toCharArray();
+		float x = startPos.getX();
+		float y = startPos.getY();
+		float z = startPos.getZ();
+		//TODO add support for color changes mid string, possibly with control characters
+		float r = color.getX();
+		float g = color.getY();
+		float b = color.getZ();
+		float a = color.getW();
 		do {
-			float x = startPos.getX();
-			float y = startPos.getY();
-			float z = startPos.getZ();
-			//TODO add support for color changes mid string, possibly with control characters
-			float r = color.getX();
-			float g = color.getY();
-			float b = color.getZ();
-			float a = color.getW();
-			dataBuffer.clear();
-			for(int i = strPos, max = (textChars.length - strPos < 128 ? textChars.length : strPos + 128); i < max; i++) {
+			int remaining = textChars.length - strPos;
+			int bytesToMap = remaining < 128 ? remaining * 144 /* 9 * 4 * 4 */ : 18432 /* 128 * 9 * 4 * 4 */;
+			
+			//Gets a ByteBuffer as a float buffer, the byte buffer being a java wrapper to a C pointer. This basically
+			//creates a memory map of the entire text rendering VBO, which will map directly to the GPU when it can
+			//GL_MAP_UNSYCNHRONIZED_BIT is used when availible because it speeds up the process
+			FloatBuffer dataBuffer;
+			if(mapBufferRange) {
+				dataBuffer = GL30.glMapBufferRange(GL15.GL_ARRAY_BUFFER, 0, bytesToMap,
+						GL30.GL_MAP_WRITE_BIT | GL30.GL_MAP_UNSYNCHRONIZED_BIT, null).asFloatBuffer();
+			} else {
+				dataBuffer = GL15.glMapBuffer(GL15.GL_ARRAY_BUFFER, GL15.GL_WRITE_ONLY, bytesToMap, null).asFloatBuffer();
+			}
+			int charsToDisplay = 0;
+			for(int i = strPos, max = (remaining < 128 ? textChars.length : strPos + 128); i < max; i++) {
 				char c = textChars[i];
 				if(displayable[c]) {
+					charsToDisplay++;
 					float tX = (c % 16) / 16F;
 					float tY = (c / 16) / 16F;
 					float width = widths[c] * charSize;
@@ -132,18 +155,23 @@ public class FontRenderer {
 					x += width;
 				}
 			}
-			dataBuffer.flip();
-			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, dataBuffer, GL15.GL_STREAM_DRAW);
-			GL11.glDrawElements(GL11.GL_TRIANGLES, textChars.length * 6, GL11.GL_UNSIGNED_SHORT, 0);
+			GL15.glUnmapBuffer(GL15.GL_ARRAY_BUFFER);
+			GL11.glDrawElements(GL11.GL_TRIANGLES, charsToDisplay * 6, GL11.GL_UNSIGNED_SHORT, 0);
+			
+			//Orphans the buffer, allowing it to be filled with more data while the previous data renders (For consecutive string draws)
+			//More info can be found here http://www.opengl.org/wiki/Buffer_Object_Streaming
+			GL15.glBufferData(GL15.GL_ARRAY_BUFFER, 18432 /* 128*9*4*4 */, GL15.GL_STREAM_DRAW);
 			strPos += 128;
 		} while(strPos < textChars.length);
 	}
 	
 	public void drawCenteredString(String text, Vec3 startPos, Vec4 color, float charSize) {
-		drawString(text, startPos.sub(new Vec3(stringWidth(text, charSize) / 2F, 0, 0)), color, charSize);
+		drawString(text, new Vec3(startPos.getX() - stringWidth(text, charSize) / 2F, startPos.getY(), startPos.getZ()), color, charSize);
 	}
 	
 	public float stringWidth(String text, float charSize) {
+		//Adjust to be accurate to the original font's point value
+		charSize *= sizeOverPt;
 		char[] textChars = text.toCharArray();
 		float width = 0;
 		for(char c : textChars) {
