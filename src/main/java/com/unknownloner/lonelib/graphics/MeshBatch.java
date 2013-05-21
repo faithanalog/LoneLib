811627b67cf14c2b03418924b8df5a788a4cc7d5
Nick Minkler <sleaker@gmail.com>
2013-05-20 20:55:26 -0700

    add ColorShader for use with ColorCubeMesh.
    add getPosition to mesh interface.
    add scale and model uniform location to MeshBatch construction.
    fix ColorCubeMesh sending color data as int, should be unsigned int.
    
    Signed-off-by: Nick Minkler <sleaker@gmail.com>

--- src/main/java/com/unknownloner/lonelib/graphics/MeshBatch.java
+++ src/main/java/com/unknownloner/lonelib/graphics/MeshBatch.java
@@ -13,82 +13,86 @@ import com.unknownloner.lonelib.math.Vec4;
 
 public class MeshBatch {
 
-	private Set<Mesh> displayList = new LinkedHashSet<Mesh>();
+    private Set<Mesh> displayList = new LinkedHashSet<Mesh>();
+    private String modelLocation;
+    private Vec3 scale;
+    private final ShaderProgram program;
 
-	private final ShaderProgram program;
+    public MeshBatch(ShaderProgram program, String modelLocation, Vec3 scale) {
+        this.program = program;
+        this.modelLocation = modelLocation;
+        this.scale = scale;
+    }
 
-	public MeshBatch(ShaderProgram program) {
-		this.program = program;
-	}
+    public void render() {
+        program.assign();
 
-	public void render() {
-		program.assign();
+        // render each mesh that's active
+        for (Mesh mesh : displayList) {
+            program.setUniform(modelLocation, Mat4.IDENTITY.translate(mesh.getPosition()).scale(scale));
+            mesh.render();
+        }
+    }
 
-		// render each mesh that's active
-		for (Mesh mesh : displayList) {
-			mesh.render();
-		}
-	}
+    /**
+     * Adds a mesh to the display set.
+     * @param mesh
+     */
+    public void addMesh(Mesh mesh) {
+        displayList.add(mesh);
+    }
 
-	/**
-	 * Adds a mesh to the display set.
-	 * @param mesh
-	 */
-	public void addMesh(Mesh mesh) {
-		displayList.add(mesh);
-	}
+    /**
+     * Removes a mesh from the display list
+     * @param mesh
+     */
+    public void removeMesh(Mesh mesh) {
+        displayList.remove(mesh);
+    }
 
-	/**
-	 * Removes a mesh from the display list
-	 * @param mesh
-	 */
-	public void removeMesh(Mesh mesh) {
-		displayList.remove(mesh);
-	}
+    /**
+     * Get the shader program this batch uses
+     * @return program
+     */
+    public ShaderProgram getProgram() {
+        return program;
+    }
 
-	/**
-	 * Get the shader program this batch uses
-	 * @return program
-	 */
-	public ShaderProgram getProgram() {
-		return program;
-	}
-	
 
-	public void setUniform(String varname, boolean value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, boolean value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, int value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, int value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, float value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, float value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, Vec2 value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, Vec2 value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, Vec3 value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, Vec3 value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, Vec4 value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, Vec4 value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, Mat4 value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, Mat4 value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, Mat3 value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, Mat3 value) {
+        program.setUniform(varname, value);
+    }
 
-	public void setUniform(String varname, Mat2 value) {
-		program.setUniform(varname, value);
-	}
+    public void setUniform(String varname, Mat2 value) {
+        program.setUniform(varname, value);
+    }
 
 }
